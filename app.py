0ee87f6c1b5ee28e4e0a512522cb5327770e6dfb
jzerilli <jzeril01@tufts.edu>
2020-03-22 18:12:33 -0400

    baseline working api

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
-
+import xgboost as xgb
+import numpy as np
 from base import Match, db
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
-app.config["DEBUG"] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://localhost/jackzerilli"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True 
@@ -19,29 +20,34 @@ parser = reqparse.RequestParser()
 parser.add_argument('p1')
 parser.add_argument('p2')
 parser.add_argument('surface')
-parser.add_argument('round')
-parser.add_argument('level')
+
+bst = xgb.Booster()  # init model
+bst.load_model('final.model')  # load data
+
 
 
 class PredictMatch(Resource):
     def get(self):
-        # use parser and find the user's query
         args = parser.parse_args()
         player1 = args['p1']
         player2 = args['p2']
-        match_round = args['round']
         surface = args['surface']
-        level = args['level']
-
-        print(Match.get_bp_converted(player1))
-        output = Match.get_wins_vs_opponent(player1, player2)
-
-        # vectorize the user's query and make a prediction
-
-        # create JSON object
-        # output = {'prediction': pred_text, 'confidence': confidence}
-        # output = 'hello'
+        player1_stats = Match.get_rank(player1), Match.get_age(player1), Match.get_height(player1), Match.get_serve_in(player1), Match.get_ace(player1), Match.get_bp_saved(player1), Match.get_serve_won(player1), Match.get_bp_converted(player1), Match.get_return_won(player1), Match.get_wins_pct(player1, None, True), Match.get_wins_pct(player1, surface, True), Match.get_wins_pct(player1, None, False), Match.get_wins_pct(player1, surface, False), Match.get_wins_vs_opponent(player1, player2), Match.get_wins_vs_opponent(player1, player2, surface)
+        player2_stats = Match.get_rank(player2), Match.get_age(player2), Match.get_height(player2), Match.get_serve_in(player2), Match.get_ace(player2), Match.get_bp_saved(player2), Match.get_serve_won(player2), Match.get_bp_converted(player2), Match.get_return_won(player2), Match.get_wins_pct(player2, None, True), Match.get_wins_pct(player2, surface, True), Match.get_wins_pct(player1, None, False), Match.get_wins_pct(player1, surface, False), Match.get_wins_vs_opponent(player2, player1), Match.get_wins_vs_opponent(player2, player1, surface)
         
+        prediction_data = []
+        for i in range(15):
+            prediction_data.append(player1_stats[i] - player2_stats[i])
+
+        prediction_data = np.array(prediction_data)
+        dtest = xgb.DMatrix(np.asmatrix(prediction_data))
+        win_probability = bst.predict(dtest)[0]
+
+        output = {'p1_name': player1, 'p1_win_prob': str(win_probability),
+                    'p2_name': player2, 'p2_win_prob': str(1 - win_probability)}
+
+
+        print(output)
         return output
 
 class PlayerList(Resource):
@@ -56,4 +62,4 @@ api.add_resource(PredictMatch, '/')
 api.add_resource(PlayerList, '/players')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- base.py
+++ base.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import and_
 from sqlalchemy import func
 from sqlalchemy import desc
+from dateutil.relativedelta import relativedelta
 #Instantiating sqlalchemy object
 
 DEFAULT_HEIGHT = 170
@@ -46,10 +47,10 @@ class Match(db.Model):
     w_ace = db.Column(db.Integer)
     w_df = db.Column(db.Integer)
     w_svpt = db.Column(db.Integer)
-    w_firstin = db.Column(db.Integer)
-    w_firstwon = db.Column(db.Integer)
-    w_secondwon = db.Column(db.Integer)
-    w_servegames = db.Column(db.Integer)
+    w_1stin = db.Column(db.Integer)
+    w_1stwon = db.Column(db.Integer)
+    w_2ndwon = db.Column(db.Integer)
+    w_svgms = db.Column(db.Integer)
     w_bpsaved = db.Column(db.Integer)
     w_bpfaced = db.Column(db.Integer)
     l_ace = db.Column(db.Integer)
@@ -67,16 +68,45 @@ class Match(db.Model):
     loser_rank_points = db.Column(db.Integer)
 
     @classmethod    
-    def get_wins_vs_opponent(cls, winner, loser):        
+    def get_wins_vs_opponent(cls, winner, loser, surface = None):
+
+        query_filter = True == True
+        if surface:        
+            query_filter = Match.surface == surface
+        
         return db.session.query(Match).filter(and_( 
             Match.winner_name == winner,
             Match.loser_name == loser,
+            query_filter
         )).count()
 
+
+
     @classmethod    
-    def get_bp_saved(cls, player):        
-        loss_matches = db.session.query(Match).with_entities(Match.l_bpsaved.label('bpsaved'), Match.l_bpfaced.label('bpfaced')).filter(Match.loser_name == player)
-        win_matches = db.session.query(Match).with_entities(Match.w_bpsaved.label('bpsaved'), Match.w_bpfaced.label('bpfaced')).filter(Match.winner_name == player)
+    def get_wins_pct(cls, player, surface, year):
+        query_filter = True == True
+        year_ago = cls.get_date() - relativedelta(years=1) 
+
+        if surface and year:
+            query_filter = and_(Match.surface == surface, Match.tourney_date >= year_ago)
+        elif surface:
+            query_filter = Match.surface == surface
+        elif year:
+            query_filter = Match.tourney_date>=year_ago
+
+        wins = db.session.query(Match).filter(and_(Match.winner_name == player , query_filter)).count()
+        losses = db.session.query(Match).filter(and_(Match.loser_name == player , query_filter)).count()
+
+        if wins and losses:
+            return wins / (wins+losses)
+        else:
+            return 0
+
+    @classmethod    
+    def get_bp_saved(cls, player):   
+        year_ago = cls.get_date() - relativedelta(years=1)     
+        loss_matches = db.session.query(Match).with_entities(Match.l_bpsaved.label('bpsaved'), Match.l_bpfaced.label('bpfaced')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
+        win_matches = db.session.query(Match).with_entities(Match.w_bpsaved.label('bpsaved'), Match.w_bpfaced.label('bpfaced')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
         all_matches = loss_matches.union_all(win_matches).subquery()
 
         bp_saved =  db.session.query(func.sum(all_matches.c.bpsaved)).first()[0]
@@ -88,9 +118,74 @@ class Match(db.Model):
             return DEFAULT_BP_SAVED
 
     @classmethod    
-    def get_bp_converted(cls, player):        
-        win_matches = db.session.query(Match).with_entities(Match.l_bpsaved.label('bpsaved'), Match.l_bpfaced.label('bpfaced')).filter(Match.winner_name == player)
-        loss_matches = db.session.query(Match).with_entities(Match.w_bpsaved.label('bpsaved'), Match.w_bpfaced.label('bpfaced')).filter(Match.loser_name == player)
+    def get_ace(cls, player):        
+        year_ago = cls.get_date() - relativedelta(years=1)
+        loss_matches = db.session.query(Match).with_entities(Match.l_ace.label('aces'), Match.l_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
+        win_matches = db.session.query(Match).with_entities(Match.w_ace.label('aces'), Match.w_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
+        all_matches = loss_matches.union_all(win_matches).subquery()
+
+        aces =  db.session.query(func.sum(all_matches.c.aces)).first()[0]
+        svpt =  db.session.query(func.sum(all_matches.c.svpt)).first()[0]
+
+        if aces and svpt:
+            return aces/svpt
+        else:
+            return DEFAULT_BP_SAVED
+
+    @classmethod 
+    def get_serve_in(cls, player):  
+        year_ago = cls.get_date() - relativedelta(years=1)      
+        loss_matches = db.session.query(Match).with_entities(Match.l_1stin.label('first_in'), Match.l_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
+        win_matches = db.session.query(Match).with_entities(Match.w_1stin.label('first_in'), Match.w_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
+        all_matches = loss_matches.union_all(win_matches).subquery()
+
+        first_in =  db.session.query(func.sum(all_matches.c.first_in)).first()[0]
+        svpt =  db.session.query(func.sum(all_matches.c.svpt)).first()[0]
+
+        if first_in and svpt:
+            return first_in/svpt
+        else:
+            return DEFAULT_BP_SAVED
+
+    @classmethod 
+    def get_serve_won(cls, player):  
+        year_ago = cls.get_date() - relativedelta(years=1)      
+        loss_matches = db.session.query(Match).with_entities(Match.l_1stwon.label('first_won'), Match.l_2ndwon.label('second_won'), Match.l_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
+        win_matches = db.session.query(Match).with_entities(Match.w_1stwon.label('first_won'),Match.w_2ndwon.label('second_won'), Match.w_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
+        all_matches = loss_matches.union_all(win_matches).subquery()
+
+        first_won =  db.session.query(func.sum(all_matches.c.first_won)).first()[0]
+        second_won =  db.session.query(func.sum(all_matches.c.second_won)).first()[0]
+        svpt =  db.session.query(func.sum(all_matches.c.svpt)).first()[0]
+
+        if first_won and second_won and svpt:
+            return (first_won + second_won)/svpt
+        else:
+            return DEFAULT_BP_SAVED
+
+    @classmethod 
+    def get_return_won(cls, player):   
+        year_ago = cls.get_date() - relativedelta(years=1)     
+        loss_matches = db.session.query(Match).with_entities(Match.l_1stwon.label('first_won'), Match.l_2ndwon.label('second_won'), Match.l_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
+        win_matches = db.session.query(Match).with_entities(Match.w_1stwon.label('first_won'),Match.w_2ndwon.label('second_won'), Match.w_svpt.label('svpt')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
+        all_matches = loss_matches.union_all(win_matches).subquery()
+
+        first_won =  db.session.query(func.sum(all_matches.c.first_won)).first()[0]
+        second_won =  db.session.query(func.sum(all_matches.c.second_won)).first()[0]
+        svpt =  db.session.query(func.sum(all_matches.c.svpt)).first()[0]
+
+        if first_won and second_won and svpt:
+            return 1 - (first_won + second_won)/svpt
+        else:
+            return DEFAULT_BP_SAVED
+    
+    
+
+    @classmethod    
+    def get_bp_converted(cls, player):
+        year_ago = cls.get_date() - relativedelta(years=1)
+        win_matches = db.session.query(Match).with_entities(Match.l_bpsaved.label('bpsaved'), Match.l_bpfaced.label('bpfaced')).filter(and_(Match.tourney_date >= year_ago, Match.winner_name == player))
+        loss_matches = db.session.query(Match).with_entities(Match.w_bpsaved.label('bpsaved'), Match.w_bpfaced.label('bpfaced')).filter(and_(Match.tourney_date >= year_ago, Match.loser_name == player))
         all_matches = loss_matches.union_all(win_matches).subquery()
 
         bp_saved =  db.session.query(func.sum(all_matches.c.bpsaved)).first()[0]
@@ -102,7 +197,7 @@ class Match(db.Model):
             return DEFAULT_BP_CONVERT
 
     @classmethod    
-    def get_rank(cls, player):        
+    def get_rank(cls, player):
         win_date, win_rank =  db.session.query(Match).with_entities(Match.tourney_date, Match.winner_rank).filter(
             Match.winner_name == player
         ).order_by(Match.tourney_date.desc()).first()
@@ -124,6 +219,11 @@ class Match(db.Model):
             return win_rank
         else:
             return loss_rank
+
+    @classmethod
+    def get_date(cls):
+        return db.session.query(Match).with_entities(Match.tourney_date).order_by(Match.tourney_date.desc()).first()[0]
+        
 
     @classmethod    
     def get_age(cls, player):        
